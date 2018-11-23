4c1bed573d7c4140b599fc4a1601a12e913464b6
jleighmo <jenny.morris@generalassemb.ly>
2018-11-23 13:05:18 -0600

    classwork

--- class18/google-maps-example/js/app.js
+++ class18/google-maps-example/js/app.js
@@ -1,9 +1,56 @@
-var map;
+ var locations = [
+    {
+      lat: 40.7484444,
+      lng: -73.9878441,
+      title: 'Empire State Building',
+    },
+    {
+      lat: 40.7516248,
+      lng: -73.9776907,
+      title: 'Chrysler Building',
+    },
+    {
+      lat: 40.7339877,
+      lng: -73.980817,
+      title: 'Flatiron Building',
+    },
+  ]
+
+  function processLocation(location) {
+    var marker = new google.maps.Marker({
+      position: {
+        lat: location.lat,
+        lng: location.lng,
+      },
+      title: location.title,
+      map: map,
+    })
+  }
+
+  var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 40.7127, lng: -74.0059},
           zoom: 16
+
         });
+
+         var marker1 = new google.maps.Marker({
+    position: {lat: 40.7127, lng: -74.0059},
+    map: map,
+    title: 'New York, NY'
+  });
+         locations.forEach(processLocation)
       }
+
+       var marker2 = new google.maps.Marker({
+    position: {lat: 45, lng: -45},
+    map: map,
+  });
+
+  var marker3 = new google.maps.Marker({
+    position: {lat: 25, lng: -75},
+    map: map,
+  });
 
       initMap()
